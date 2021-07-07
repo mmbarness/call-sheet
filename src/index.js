237fd9ae34b56f7ebd1cde5e17da7e4fb88444a4
matthew barnes <011mbarnes@gmail.com>
2021-07-07 07:59:42 -0400

    yep

--- src/index.js
+++ src/index.js
@@ -36,5 +36,5 @@ const pageLoad = (directors) => {
 
 }
 
-pageLoad(['michael mann' ])
-// pageLoad(['michael mann', 'claire denis', 'martin scorsese', 'Spike Lee', 'kathryn bigelow' ])
+// pageLoad(['michael mann' ])
+pageLoad(['michael mann', 'claire denis', 'martin scorsese', 'Spike Lee', 'kathryn bigelow' ])
